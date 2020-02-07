6a3f104de591ec8aa4409c41d62227a1cc1f8e60
HassanKassem98 <60794189+HassanKassem98@users.noreply.github.com>
2020-02-07 21:13:00 +0200

    Add files via upload

---
+++ FirstSession.c
@@ -0,0 +1,50 @@
+
+
+/**
+ * main.c
+ */
+ 
+ 
+#define REG volatile unsigned long int *
+#define F_BASE 0x4005D000
+#define FB(offest) F_BASE + offest
+#define PIN 1
+
+
+REG RCC = 0x400FE060;
+REG GPIOHBCTL = 0x400FE06C;
+REG RCGCGPIO = 0x400FE608;
+REG GPIODIRF = F_BASE;
+
+REG GPIOAFSEL_F = FB(0x420);
+REG GPIODB8R_F = F_BASE + 0x508;
+REG GPIODEN_F = FB(0x51C);
+REG GPIODATA_F1 = FB(0x00);
+
+
+int main(void)
+
+{
+    // Run-Mode Clock Configuration: Internal CLK
+
+    *RCC |= (1 << 11);
+    *RCC |= (1 << 22);
+    *RCC |= (1 << 11);
+    *RCC |= (1 << 4);
+    *RCC &= ~(1 << 5);
+    *RCC |= (1 << 13);
+
+
+    // GPIO Configuration
+
+    *GPIOHBCTL |= (1 << 5); // Enable PORT F AHB
+    *RCGCGPIO |= (1 << 5); // Enable CLK
+    *GPIODIRF |= (1 << 1); // Enable PORT F
+    *GPIOAFSEL_F &= ~(1 << 1); // GPIO Mode
+    *GPIODB8R_F |= (1 << 1);
+    *GPIODEN_F |= (1 << PIN);
+    *GPIODATA_F1 = 0xFF;
+
+
+    return 0;
+}
